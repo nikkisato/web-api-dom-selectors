6a4c6b9e6104a25550ce8b47b10daad891cdedc2
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-06-12 20:09:40 -0700

    attempting parser

---
+++ javascript/parser.js
@@ -0,0 +1,22 @@
+function fetchStuff() {
+  fetch(window.location.url)
+    .then((res) => {
+      return res.text();
+    })
+    .then((text) => {
+      const parser = new DOMParser();
+      console.log(parser);
+      const fetchedDom = parser.parseFromString(text, "text/html");
+      document.createElement("h1");
+      const heading = fetchedDom.body.querySelector("h1");
+
+      const div = document.createElement("div");
+      div.append(heading);
+      div.classList.add("parser-borders");
+      document.querySelector("body").appendChild(div);
+    });
+}
+
+setInterval(() => {
+  fetchStuff();
+}, 3000);
