50303e652a696751bae94345085f611b43cfc59f
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-06-12 20:35:26 -0700

    appending text instead of parser?

--- javascript/parser.js
+++ javascript/parser.js
@@ -6,14 +6,14 @@ function fetchStuff() {
     })
     .then((text) => {
       console.log("text", text);
-      const parser = new DOMParser();
-      console.log(parser);
-      const fetchedDom = parser.parseFromString(text, "text/html");
-      document.createElement("h1");
-      const heading = fetchedDom.body.querySelector("h1");
+      //   const parser = new DOMParser();
+      //   console.log(parser);
+      //   const fetchedDom = parser.parseFromString(text, "text/html");
+      //   document.createElement("h1");
+      //   const heading = fetchedDom.body.querySelector("h1");
 
       const div = document.createElement("div");
-      div.append(heading);
+      div.append(text);
       div.classList.add("parser-borders");
       document.querySelector("body").appendChild(div);
     });
