d7dc29ea6792f5e433595966e902d8145500a651
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-06-12 21:20:03 -0700

    color dropper works

---
+++ javascript/eyeDropper.js
@@ -0,0 +1,22 @@
+document.getElementById("start-button").addEventListener("click", () => {
+  const resultElement = document.getElementById("result");
+
+  if (!window.EyeDropper) {
+    resultElement.textContent = "Your browser does not support the EyeDropper API";
+    return;
+  }
+
+  const eyeDropper = new EyeDropper();
+
+  eyeDropper
+    .open()
+    .then((result) => {
+      resultElement.textContent = result.sRGBHex;
+      resultElement.style.backgroundColor = result.sRGBHex;
+    })
+    .catch((e) => {
+      resultElement.textContent = e;
+    });
+});
+
+//https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper
