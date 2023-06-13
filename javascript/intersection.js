517dec903e9239298f7d1482b6f5c19873f7e4c7
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-06-12 20:30:40 -0700

    put back the //

--- javascript/intersection.js
+++ javascript/intersection.js
@@ -1,13 +1,13 @@
-/Intersection Observer
-/https:/wesbos.com/javascript/06-serious-practice-exercises/scroll-events-and-intersection-observer
-/ Listen for scroll position. Animate something when the user crosses a threshold
+//Intersection Observer
+//https:/wesbos.com/javascript/06-serious-practice-exercises/scroll-events-and-intersection-observer
+// Listen for scroll position. Animate something when the user crosses a threshold
 
-/ Creating a div element
+// Creating a div element
 const bodyElement2 = document.querySelector("body");
 const div2Element = document.createElement("div");
 div2Element.classList.add("container", "conditions");
 
-/ attaching content to div
+//attaching content to div
 div2Element.innerHTML = `<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus <strong class="watch">Watch for Me</strong> PageMaker including versions of Lorem Ipsum.
@@ -26,23 +26,22 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like  Aldus PageMaker including versions of Lorem Ipsum.
   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>`;
 
-/ appending div to body
+// appending div to body
 document.querySelector("body").appendChild(div2Element);
 
-/creating accept button
+//creating accept button
 const button2Element = document.createElement("button");
 button2Element.innerText = "Click to Accept";
 button2Element.classList.add("conditions-button", "accept");
 button2Element.disabled = true;
 div2Element.appendChild(button2Element);
 
-/ querySelecting for observer
+// querySelecting for observer
 const conditionSection = document.querySelector(".conditions");
 const watch = document.querySelector(".watch");
 const button = document.querySelector(".accept");
 
-
-/ Creating a div element
+// Creating a div element
 function obCallback(payload) {
   console.log(payload[0].intersectionRatio);
   if (payload[0].intersectionRatio === 1) {
--- javascript/mutation.js
+++ javascript/mutation.js
@@ -1,24 +1,24 @@
-/ Create a document object and append to the body
+//Create a document object and append to the body
 const divElement = document.createElement("div");
 divElement.classList.add("container");
 document.querySelector("body").appendChild(divElement);
 
-/create a button and attach it within the div
+//create a button and attach it within the div
 const buttonElement = document.createElement("button");
 buttonElement.classList.add("button");
 buttonElement.innerText = "Click Me";
 divElement.appendChild(buttonElement);
 
-/ Mutation Observer
-/ On Click change the background color of the divElement
+//Mutation Observer
+//On Click change the background color of the divElement
 buttonElement.addEventListener("click", changeBackgroundColor);
 
 function changeBackgroundColor(event) {
   event.preventDefault();
   console.log("Clicked");
 
-  /generates a random hex color string
-  /https:/www.educative.io/answers/how-to-generate-a-random-color-in-javascript
+  //generates a random hex color string
+  //https:/www.educative.io/answers/how-to-generate-a-random-color-in-javascript
   const randColor = () => {
     return (
       "#" +
