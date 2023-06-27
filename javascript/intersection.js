5b5ff6faaecb84d94cbbdc5076d79f34850b16ab
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-06-26 22:33:42 -0700

    finished intersection

--- javascript/intersection.js
+++ javascript/intersection.js
@@ -2,27 +2,6 @@
 // https:/wesbos.com/javascript/06-serious-practice-exercises/scroll-events-and-intersection-observer
 // Listen for scroll position. Animate something when the user crosses a threshold
 
-// Creating a div element
-const bodyElement2 = document.querySelector("body");
-const div2Element = document.createElement("div");
-div2Element.classList.add("container", "conditions");
-
-for (let i = 0; i < 100; i++) {
-  const text = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
-
-  div2Element.append(text);
-}
-
-// appending div to body
-document.querySelector("body").appendChild(div2Element);
-
-//creating accept button
-const button2Element = document.createElement("button");
-button2Element.innerText = "Click to Accept";
-button2Element.classList.add("conditions-button", "accept");
-button2Element.disabled = true;
-div2Element.appendChild(button2Element);
-
 // querySelecting for observer
 const conditionSection = document.querySelector(".conditions");
 const watch = document.querySelector(".watch");
@@ -50,3 +29,23 @@ ob.observe(conditionSection.lastElementChild);
 // vinoshopper as we scroll animate left and animate right
 // things appear
 //https://www.freecodecamp.org/news/scroll-animations-with-javascript-intersection-observer-api/
+
+// again this guy helped again https://www.youtube.com/watch?v=2IbRtjez6ag
+
+const cards = document.querySelectorAll(".card");
+
+const observer2 = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+      if (entry.isIntersecting) observer2.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
+
+cards.forEach((card) => {
+  observer2.observe(card);
+});
